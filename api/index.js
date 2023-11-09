6d08554112100784e57c6c9b205af94c9aab6573
yusufobr <yousarsif80@gmail.com>
2023-11-09 22:53:53 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,24 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });
