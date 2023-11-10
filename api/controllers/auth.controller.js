fb130af8432f6216b61d538b95523283b206cbd9
yusufobr <yousarsif80@gmail.com>
2023-11-10 20:24:28 +0100

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,46 @@
 import User from "../models/user.model.js";
-import hashPassword from "../utils/hashPassword.js";
+import errorHandler from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({
     username,
     email,
-    password: await hashPassword(password),
+    password: hashedPassword,
   });
+  console.log(hashedPassword);
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully!" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(400, "Invalid credentials!"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const userInfos = {
+      id: validUser._id,
+      username: validUser.username,
+      email: validUser.email,
+    };
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(userInfos);
   } catch (error) {
     next(error);
   }
--- api/utils/hashPassword.js
+++ api/utils/hashPassword.js
@@ -1,10 +1,9 @@
 import bcrypt from 'bcrypt';
-const saltRounds = 10; // This is the cost factor for the hashing function.
 
 async function hashPassword(password) {
     try {
-        const salt = await bcrypt.genSalt(saltRounds);
-        const hash = await bcrypt.hash(password, salt);
+        const hash = await bcrypt.hash(password, 10);
+        console.log(hash);
         return hash;
     } catch (error) {
         console.error(error);
