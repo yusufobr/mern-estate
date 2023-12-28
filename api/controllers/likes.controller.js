df5bc6f96e7366fcb065c246a5542944ccb21865
yusufobr <yousarsif80@gmail.com>
2023-12-28 19:06:49 +0100

    Check if the like already exists

--- api/controllers/likes.controller.js
+++ api/controllers/likes.controller.js
@@ -3,10 +3,18 @@ import Like from "../models/likes.model.js";
 export const addLike = async (req, res, next) => {
   try {
     const { user, listing } = req.body;
+
+    const existingLike = await Like.findOne({ user, listing });
+
+    if (existingLike) {
+      return res.status(400).json("You already liked this listing");
+    }
+
     const like = await Like.create({
       user,
       listing,
     });
+    
     return res.status(201).json(like);
   } catch (error) {
     next(error);
