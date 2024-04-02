83ef58f457a9cc2f0b96a79dcda1afa6f174f9a3
yusufobr <yousarsif80@gmail.com>
2024-04-02 21:42:14 +0000

    create Style Pagargraph component

---
+++ client/src/utils/StylizeParagraph.tsx
@@ -0,0 +1,18 @@
+// StylizeParagraph.tsx
+import React from 'react';
+
+const stylizeParagraph = (paragraph: string): JSX.Element => {
+    const points = paragraph.split('. ');
+
+    // Map each point to a JSX element with a new line after it
+    const jsxElements = points.map((point, index) => (
+      <React.Fragment key={index}>
+        <span>{point}.</span>
+        <br />
+      </React.Fragment>
+    ));
+  
+    return <div>{jsxElements}</div>;
+}
+
+export default stylizeParagraph;
