68bb36c6154721441f01c4c0d3b4208d2a139aaf
yusufobr <yousarsif80@gmail.com>
2024-04-06 03:59:58 +0000

    add comment pagination UI & Scroll

--- client/src/components/Comments.tsx
+++ client/src/components/Comments.tsx
@@ -9,17 +9,20 @@ const Comments = () => {
 
   const [comment, setComment] = useState<string>("");
   const [comments, setComments] = useState<any[]>([]);
+  const [commentLimit, setCommentLimit] = useState<number>(5);
   const navigate = useNavigate();
 
   useEffect(() => {
     fetchComments();
-  }, []);
+  }, [commentLimit]);
 
   const fetchComments = async () => {
     try {
-      const res = await axios.get(`/api/comment/get?listingId=${id}&limit=5`);
-      setComments(res.data);
+      const res = await axios.get(
+        `/api/comment/get?listingId=${id}&limit=${commentLimit}`
+      );
       // console.log(res.data);
+      setComments(res.data);
     } catch (error) {
       console.error(error);
     }
@@ -50,50 +53,68 @@ const Comments = () => {
       {comments.length === 0 && (
         <div className="text-center">No comments yet</div>
       )}
-      {comments.map((comment, index) => (
-        <>
-          <div key={comment._id} className="">
-            <div className="w-10 h-10 rounded-full float-left mr-2">
-              <img
-                src={
-                  comment.userDetails?.profilePicture ||
-                  "https://via.placeholder.com/150"
-                }
-                alt={comment.userDetails.username}
-                className="w-10 h-10 rounded-full object-cover"
-              />
+      <div className="flex flex-col gap-1 overflow-auto h-80">
+        {comments.map((comment, index) => (
+          <>
+            <div key={comment._id} className="">
+              <div className="w-10 h-10 rounded-full float-left mr-2">
+                <img
+                  src={
+                    comment.userDetails?.profilePicture ||
+                    "https://via.placeholder.com/150"
+                  }
+                  alt={comment.userDetails.username}
+                  className="w-10 h-10 rounded-full object-cover"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <p className="text-sm text-gray-500 font-extrabold -mb-1">
+                  {comment.userDetails.username}
+                </p>
+                <p className="text-sm">{comment.comment}</p>
+              </div>
             </div>
-            <div className="flex flex-col gap-1">
-              <p className="text-sm text-gray-500 font-extrabold -mb-1">
-                {comment.userDetails.username}
-              </p>
-              <p className="text-sm">{comment.comment}</p>
+            <div className="flex justify-end">
+              <span className="text-xs text-gray-400">
+                {new Date(comment.createdAt).toLocaleDateString()}
+              </span>
             </div>
-          </div>
-          {index !== comments.length - 1 && (
-            <div className="w-80 h-[1px] bg-gray-200"></div>
-          )}
-        </>
-      ))}
+            {index !== comments.length - 1 && (
+              <div className="w-80 h-[1px] bg-gray-200"></div>
+            )}
+          </>
+        ))}
+      </div>
+
+      <div className="flex justify-center">
+        <button
+          onClick={() => setCommentLimit(commentLimit + 5)}
+          className="text-blue-500 font-semibold"
+        >
+          More
+        </button>
+      </div>
       <div className="flex flex-col gap-2 py-3">
         <div className="text-sm font-semibold text-center text-gray-800">
           Add a comment :
         </div>
-        <form onSubmit={addComment} className="flex flex-col gap-2">
+        <form onSubmit={addComment} className="flex flex-col gap-3 items-end">
           <textarea
             value={comment}
             placeholder="Add a comment"
             cols={30}
             rows={2}
-            className="border rounded p-2"
+            className="border rounded p-2 w-full"
             onChange={(e) => setComment(e.target.value)}
           ></textarea>
-          <button
-            type="submit"
-            className="bg-gray-600 text-white font-semibold py-2 rounded"
-          >
-            comment
-          </button>
+          <div>
+            <button
+              type="submit"
+              className="bg-gray-600 px-3 text-white font-semibold py-2 rounded"
+            >
+              comment
+            </button>
+          </div>
         </form>
       </div>
     </div>
